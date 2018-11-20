66d874f97089907d1e68cdfa2cb268c90b2f7a9a
alexander <mail@fomalhaut.su>
2018-11-20 11:32:25 +0300

    Rare methods fix

--- visitstat/middleware/visits.py
+++ visitstat/middleware/visits.py
@@ -7,15 +7,23 @@ from visitstat.models import Visit
 
 class StatisticsMiddleware(object):
     def process_response(self, request, response):
-        visit = Visit(
-            datetime=datetime.now(pytz.utc),
-            ip=request.META.get('REMOTE_ADDR'),
-            method=request.method,
-            url=request.path,
-            referer=request.META.get('HTTP_REFERER'),
-            querystring=getattr(request, request.method).urlencode(),
-            status=response.status_code,
-            reason=response.reason_phrase
-        )
-        visit.save()
+        try:
+            Visit.objects.create(
+                datetime=datetime.now(pytz.utc),
+                ip=request.META.get('REMOTE_ADDR'),
+                method=request.method,
+                url=request.path,
+                referer=request.META.get('HTTP_REFERER'),
+                querystring=self._get_querystring(request),
+                status=response.status_code,
+                reason=response.reason_phrase
+            )
+        except:
+            pass
         return response
+
+    def _get_querystring(self, request):
+        if request.method in ['GET', 'POST']:
+            return getattr(request, request.method).urlencode()
+        else:
+            return None
